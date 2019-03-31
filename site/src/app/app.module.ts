42254bc8a99140545d58a5240b9197e1d9d94e02
FernandoSBento <fernando_bento2@hotmail.com>
2019-03-30 23:36:41 -0300

    aula 01 e 02

---
+++ site/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import {FormsModule} from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import {HomeComponent} from './home/home.component'
+import {RodapeComponent} from './rodape/rodape.component';
+import {SobreComponent} from './sobre/sobre.component';
+import {Erro404Component} from './erro404/erro404.component';
+import {SomaComponent} from './soma/soma.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent, HomeComponent, RodapeComponent,
+    SobreComponent, Erro404Component, SomaComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
